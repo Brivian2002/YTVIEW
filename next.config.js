4a016225a74e11192f5e848da8e9ac1a0867ffeb
Bright Dumashie <161515806+Brivian2002@users.noreply.github.com>
2026-03-29 22:11:37 -0400

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // If you are using static export, add:
+  // output: 'export',
+};
+
+module.exports = nextConfig;
