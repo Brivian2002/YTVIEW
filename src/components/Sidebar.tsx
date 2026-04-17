c0d83407d4d3e5c3d3da793f7630956c44a62bbb
Bright Dumashie <161515806+Brivian2002@users.noreply.github.com>
2026-04-17 12:45:24 +0000

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -148,6 +148,8 @@ export default function Sidebar() {
                 <ChevronLeft className="w-4 h-4" />
               )}
             </Button>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7744791430316817"
+     crossorigin="anonymous"></script>
           </div>
 
           {/* Navigation */}
